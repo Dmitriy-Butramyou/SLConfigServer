995255c8670bb90158261a60995b1f76f21a2d46
D_Butramyou <d.butramyou@gmail.com>
2021-08-10 15:27:50 +0300

    Initial commit

---
+++ src/main/java/com/butramyou/SLConfigServer/SlConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.butramyou.SLConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SlConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/butramyou/SLConfigServer/SlConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.butramyou.SLConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
