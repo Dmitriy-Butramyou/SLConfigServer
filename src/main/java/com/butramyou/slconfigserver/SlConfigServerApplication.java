914e4076c9f72fc8e2feb369410c4db2a0b73a12
D_Butramyou <d.butramyou@gmail.com>
2021-08-10 21:38:54 +0300

    Refactoring

--- src/main/java/com/butramyou/slconfigserver/SlConfigServerApplication.java
+++ src/main/java/com/butramyou/slconfigserver/SlConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.butramyou.SLConfigServer;
+package com.butramyou.slconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/butramyou/slconfigserver/SlConfigServerApplicationTests.java
+++ src/test/java/com/butramyou/slconfigserver/SlConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.butramyou.SLConfigServer;
+package com.butramyou.slconfigserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
